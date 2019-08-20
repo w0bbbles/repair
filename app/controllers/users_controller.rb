fee08393aabf535884bff2f4758581e59e8f6d5c
BenBear93 <kjieun1993@gmail.com>
2019-08-20 14:32:02 +0800

    edited controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  def index
 
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 
 end
