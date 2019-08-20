451241944852c7bf31b779ce89d8b4fae2084e0a
BenBear93 <kjieun1993@gmail.com>
2019-08-20 16:53:02 +0800

    testing out show.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_20_075831) do
 
   # These are extensions that must be enabled in order to support this database
@@ -28,6 +27,11 @@ ActiveRecord::Schema.define(version: 2019_08_20_075831) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "landingpages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,10 +45,4 @@ ActiveRecord::Schema.define(version: 2019_08_20_075831) do
   end
 
   add_foreign_key "items", "users"
-  create_table "landingpages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
-  end
-
 end
