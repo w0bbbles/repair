42d293794042b0a91bbc85d10f22a87947b3cd9b
gitwenlei <ngwenlei@gmail.com>
2019-08-20 17:20:48 +0800

    Added seeds file for fake content

--- db/migrate/20190820053302_items.rb
+++ db/migrate/20190820053302_items.rb
@@ -2,7 +2,6 @@ class Items < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
-      #t.references :user
       t.text :description
       t.boolean :repairing
       t.boolean :completed
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+items = Item.create(
+  [
+    {
+    name: "tv",
+    description: "cracked screen & lines on screen",
+    repairing: false,
+    completed: false,
+    repairer: 3,
+    image: "https://media.karousell.com/media/photos/products/2019/08/11/tv_1565495782_f9efa008_progressive.jpg"
+    },
+    {
+    name: "camera",
+    description: "Shutter not working",
+    repairing: false,
+    completed: false,
+    repairer: 3,
+    image: "https://media.karousell.com/media/photos/products/2019/08/17/camera_1566046635_87d114ec_progressive.jpg"
+    },
+    {
+    name: "hairdryer",
+    description: "Burning hot after 1-min",
+    repairing: false,
+    completed: false,
+    repairer: 2,
+    image: "https://media.karousell.com/media/photos/products/2019/08/19/philips_ionic_hairdryer_1566202657_85a61ce3_progressive.jpg"
+    }
+  ]
+)
