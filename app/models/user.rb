0d015936e4bad5862bf0a31b7f70c12de0ffe264
w0bbbles <wobblebeat@gmail.com>
2019-08-20 14:00:50 +0800

    added devise files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  has_and_belongs_to_many :items
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :item
 end
