261d5bb4271bef70364bd3eaf7ddd1b60558fbcd
w0bbbles <wobblebeat@gmail.com>
2019-08-21 09:58:04 +0800

    more seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,3 +50,23 @@ items = Item.create(
     }
   ]
 )
+
+users = User.create(
+  [
+    {
+      email: "keith@email.com"
+      password: "123456"
+    },
+    {
+      email: "ty@email.com"
+      password: "123456"
+    },
+    {
+      email: "wenlei@email.com"
+      password: "123456"
+    },
+    {
+      email: "weide@email.com"
+      password: "123456"
+    }
+    }])
