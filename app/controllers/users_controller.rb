19a25494d1c1bb4006cca290f0b74d7414cdfa1e
w0bbbles <wobblebeat@gmail.com>
2019-08-22 17:18:40 +0800

    made a test file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,4 +45,8 @@ class UsersController < ApplicationController
 
   end
 
+  def test
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landingpage#index'
 
+  get '/users/test' => 'users#test', as: 'test'
+
   resources :landingpage
   resources :devise
   resources :items
