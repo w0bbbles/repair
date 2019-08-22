5c3b9bf3321477d9514d9afac2806e324587ad4b
Yew <limtheanyew@gmail.com>
2019-08-22 17:05:39 +0800

    Done Items Preview in landingpage

--- app/controllers/landingpage_controller.rb
+++ app/controllers/landingpage_controller.rb
@@ -1,4 +1,9 @@
 class LandingpageController < ApplicationController
     def index
+        # @items = Item.all
+    puts "****************"
+    puts "USEEEEER"
+    puts current_user.id
+    @items = Item.where(repairing: false, completed: false)
     end
 end
